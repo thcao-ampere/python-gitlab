ed333f86ca64aab081b564c5336e1973c50c6c62
John L. Villalovos <john@sodarock.com>
2022-07-03 09:27:55 -0700

    fix: ensure path elements are escaped
    Ensure the path elements that are passed to the server are escaped.
    For example a "/" will be changed to "%2F"
    
    Closes: #2116

--- gitlab/base.py
+++ gitlab/base.py
@@ -19,6 +19,7 @@ import importlib
 from types import ModuleType
 from typing import Any, Dict, Iterable, NamedTuple, Optional, Tuple, Type
 
+import gitlab
 from gitlab import types as g_types
 from gitlab.exceptions import GitlabParsingError
 
@@ -310,10 +311,12 @@ class RESTManager(object):
         if self._parent is None or not self._from_parent_attrs:
             return path
 
-        data = {
-            self_attr: getattr(self._parent, parent_attr, None)
-            for self_attr, parent_attr in self._from_parent_attrs.items()
-        }
+        data: Dict[str, Optional[gitlab.utils.EncodedId]] = {}
+        for self_attr, parent_attr in self._from_parent_attrs.items():
+            if not hasattr(self._parent, parent_attr):
+                data[self_attr] = None
+                continue
+            data[self_attr] = gitlab.utils.EncodedId(getattr(self._parent, parent_attr))
         self._parent_attrs = data
         return path % data
 
